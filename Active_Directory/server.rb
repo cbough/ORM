f96c3e46c48efafb9468187e3ad9b0d48ceb4e12
Christopher Bough <christopherbough85@gmail.com>
2019-07-03 14:16:10 -0400

    added Active Record Directory

---
+++ Active_Directory/server.rb
@@ -0,0 +1,61 @@
+require "sinatra/activerecord"
+require "sinatra"
+
+ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: "./database.sqlite3")
+
+set :database, {adapter: "sqlite3", database: "./database.sqlite3"}
+
+enable :sessions
+
+class User < ActiveRecord::Base
+end
+
+get '/' do
+  puts 'running'
+  erb :'users/home'
+end
+
+get '/signup' do
+  @user = User.new
+  erb :'users/signup'
+end
+
+post '/signup' do
+  @user = User.new(params)
+  if @user.save
+    p "#{@user.first_name} was saved to the database"
+    redirect '/thanks'
+  end
+end
+
+get '/thanks' do
+  erb :'users/thanks'
+end
+
+get '/login' do
+  if session[:user_id]
+    redirect '/'
+  else
+    erb :'users/login'
+  end
+end
+
+post '/login' do
+  given_password = params['password']
+  user = User.find_by(email: params['email'])
+  if user
+    if user.password == given_password
+      p 'User authenticated successfully'
+      session[:user_id] = user.id
+    else
+      p "invalid email or password"
+    end
+  end
+end
+
+# Delete Request
+post '/logout' do
+  session.clear
+  p 'user logged out successfully'
+  redirect '/'
+end
